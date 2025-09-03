c02401d8969226235d0ae08d93bdc047e8c7710f
67112533-promax <67112533@dpu.ac.th>
2025-09-03 12:38:30 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from myfunc import add
+a = float(input("กรอกตัวเลข A: "))
+b = float(input("กรอกตัวเลข B: "))
+result = add(a, b)
+print(f"ผลลัพธ์ของ {a} + {b} = {result}")
---
+++ myfunc.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    a = float(input("กรอกค่า A: "))
+    b = float(input("กรอกค่า B: "))
+    print("ผลลัพธ์ของ A + B =", add(a, b))
